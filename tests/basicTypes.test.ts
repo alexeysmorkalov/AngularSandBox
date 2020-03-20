9b1a7fe8628be852abb8bfb1dd4cd56cc7b7b0e0
AlexeySmorkalov <magen-ta@yandex.ru>
2020-03-21 00:12:55 +0300

    typescript basic types

---
+++ tests/basicTypes.test.ts
@@ -0,0 +1,140 @@
+import { expect } from 'chai';
+
+describe('TypeScript types', () => {
+    let t : boolean = true;
+    let f : boolean = false;
+    it('Boolean, true', () => {
+        expect(true).to.be.true;
+    });
+
+    it('Boolean, false', () => {
+        expect(false).to.eq(f);
+    });
+
+    it ('Number decumal', () => {
+        let n : number = 10;
+        expect(n).to.be.greaterThan(n-1);
+    });
+
+    it ('string template', () => {
+        let n : number = 1;
+        let s : string = `${ n + 1 }`;
+        expect(s).to.be.equal(`2`); 
+    });
+
+    it ('array declaration generic', () => {
+        let arr: Array<number> = [1, 2, 3];
+        expect(arr.length).to.be.equal(3);
+    });
+
+    it ('array declaration decl', () => {
+        let arr: number[] = [1, 2, 3];
+        expect(arr.length).to.be.equal(3);
+    });
+
+    it ('touple 1', () => {
+        let v1: [number, string];
+        let v2: [number, string];
+        v1 = [1, '1'];
+        v2 = v1;
+        expect(v1).to.be.equal(v2);
+    });
+
+    it ('touple 2', () => {
+        let v1: [number, string, number];
+        let v2: [number, string, number];
+        v1 = [1, '1', 1];
+        v2 = v1;
+        expect(v1).to.be.equal(v2);
+    });
+
+    it ('touple get', () => {
+        let v1: [number, string, number] = [1, '1', 1];
+        expect(v1[0]).to.be.equal(v1[2]);
+    });
+
+    it ('touple set', () => {
+        let v1: [number, string, number] = [1, '1', 1];
+        let n = 'n';
+        v1[1] = n;
+        expect(v1[1]).to.be.equal(n);
+    });
+
+    it ('enum basic', () => {
+        enum Alignment {Left, Center, Right};
+        let align : Alignment = Alignment.Left;
+        expect(align.valueOf()).to.be.equal(0);
+    });
+    
+    it ('enum index 0', () => {
+        enum Alignment {Left = 1, Center, Right};
+        let align : Alignment = Alignment.Left;
+        expect(align.valueOf()).to.be.equal(1);
+    });
+
+    it ('enum index 1', () => {
+        enum Alignment {Left, Center, Right = 4};
+        let align : Alignment = Alignment.Left;
+        expect(Alignment.Left).to.be.equal(0);
+        expect(Alignment.Right).to.be.equal(4);
+    });    
+    
+    it ('enum num string', () => {
+        enum Alignment {Left, Center, Right = 4};
+        let alignString : string = Alignment[0];
+        let alignNumber : number = Alignment.Left.valueOf();
+        expect(alignString).to.be.equal('Left');
+        expect(alignNumber).to.be.equal(0);
+    });
+
+    it ('enum num string', () => {
+        enum Alignment {Left, Center, Right = 4};
+        let alignString : string = Alignment[0];
+        let alignNumber : number = Alignment.Left.valueOf();
+        expect(alignString).to.be.equal('Left');
+        expect(alignNumber).to.be.equal(0);
+    });
+
+    it ('any', () => {
+        let s : any = 0;
+        expect(s).to.be.not.string;
+        s = true;
+        expect(s).to.be.true;
+    });
+
+    it ('null & undefined', () => {
+        let a: any = 1;
+        a = null;
+        expect(a).to.be.null;
+        expect(a).not.to.be.undefined;
+        a = undefined;
+        expect(a).to.be.undefined;
+        expect(a).not.to.be.null;
+    });
+
+    it ('union type', () => {
+        let a: string | null = '1';
+        a = null;
+        expect(a).to.be.null;
+        expect(a).not.to.be.undefined;
+        a = '1';
+        expect(a).to.be.string;
+        expect(a).not.to.be.null;
+    });
+
+    
+    it ('never', () => {
+        function n () : never {
+            throw new Error('Err');
+        }
+
+        expect(()=>n()).throws('Err');
+    });
+
+    //BigInt literals are not available when targeting lower than ES2020.ts(2737)
+    /*it ('bigint', () => {
+        let n: bigint = 100n;
+        expect(n).to.be.equal(100n - 1n);
+    });*/
+    
+})
