1701ad9c4ad3e218a10a0566b200a35a2853cb76
AlexeySmorkalov <magen-ta@yandex.ru>
2020-04-03 23:43:38 +0300

    generic types

--- tests/generics.test.ts
+++ tests/generics.test.ts
@@ -1,6 +1,6 @@
 import {expect} from 'chai';
 
-describe('generics', () => {
+describe('generics intro', () => {
     it ('the most generic generic :)', () => {
         function MySelf<T>(arg: T): T {
             return arg;
@@ -28,5 +28,61 @@ describe('generics', () => {
         const res = CalcArray<number>(Array<number>(1,2));
 
         expect(res.join(',')).to.be.equal('2,1');
-    })
-})
+    });
+});
+
+describe('generics types', () => {
+
+    function Calc<T>(arg: T): T {
+        return arg;
+    }
+
+    it ('generic type1', () => {
+
+        let calc: <T>(arg: T) => T = Calc;
+
+        const res = calc("1");
+
+        expect(res).to.be.equal("1");
+    });
+
+    it ('generic type2', () => {
+        let calc: <U>(arg: U) => U = Calc;
+
+        const res = calc(1);
+
+        expect(res).to.be.equal(1);
+    });
+
+    it ('generic type3', () => {
+        let calc : {<T>(arg: T): T } = Calc;
+
+        const res = calc(null);
+
+        expect(res).to.be.null;
+    });
+
+    it ('generic interface', () => {
+        interface CalcEngine {
+            <T>(arg: T): T;
+        }
+
+        let calc: CalcEngine = Calc;
+
+        const res = calc(0);
+
+        expect(res).to.be.equal(0);
+    });
+
+    it ('generic interface2', () => {
+        interface CalcEngine<T> {
+            (arg: T): T;
+        }
+
+        let calc: CalcEngine<number> = Calc;
+
+        const res = 2;
+
+        expect(res).to.be.equal(2);
+    });
+});
