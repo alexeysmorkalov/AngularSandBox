f240885397644ddbbe43aa8f522221917cdea99d
Alexey Smorkalov <magen-ta@yandex.ru>
2020-08-06 12:16:07 +0300

    Array object tests

--- tests/JavaScript/obj.array.test.js
+++ tests/JavaScript/obj.array.test.js
@@ -69,11 +69,19 @@ describe('built-in Array object', () => {
         expect(arr3.length).to.be.equal(arr1.length + 1);
         expect(arr3[0]).to.be.equal(arr1[0]);
         expect(arr3[arr1.length]).to.be.equal(arr2);
+        // result was a new array
+        expect(arr1).not.to.be.equal(arr3);
+        expect(arr2).not.to.be.equal(arr3);
     });
 
+    //The copyWithin() method shallow copies part of an array to another location in the same array and returns it without modifying its length.
     it ('Array.copyWithin()', () => {
-        const arr = [1, 2, 3].copyWithin(-1);
+        let arr0 = [1, 2, 3];
+        let arr = arr0.copyWithin(-1);
         expect(arr).to.be.eql([1, 2, 1]);
+        
+        // result was the same array
+        expect(arr).to.be.equal(arr0);
     });
 
     it ('Array.entries()', () => {
@@ -84,5 +92,23 @@ describe('built-in Array object', () => {
         expect(index2).to.be.equal(1);
         expect(element1).to.be.equal("a");
         expect(element2).to.be.equal("b");
+    });
+
+    it ('Array.every()', () => {
+        const arr = Array.from("abcde");
+        let res = arr.every(x => typeof x === "string");
+        expect(res).to.be.true;
+    });
+
+    // The fill() method changes all elements in an array to a static value, from a start index (default 0) to an end index (default array.length). 
+    // It returns the modified array.
+    it ('Array.fill()', () => {
+        let array1 = [1, 2, 3, 4];
+        let array2 = array1.fill(0, 0);
+
+        expect(array2.every(x => x === 0)).to.be.true;
+        // result was the same array
+        expect(array1).to.be.equal(array2);
+
     });
 });
